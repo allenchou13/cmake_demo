d935492482099763ad4e9c7cffa07b62e3a76450
Gitea <gitea@fake.local>
2020-07-20 00:45:50 +0800

    add codes

---
+++ trial_lib.cpp
@@ -0,0 +1,9 @@
+#include "trial_lib.h"
+
+namespace trial_lib
+{
+    int add(int a, int b)
+    {
+        return a + b;
+    }
+} // namespace trial_lib
---
+++ trial_lib.h
@@ -0,0 +1,5 @@
+
+namespace trial_lib
+{
+    int add(int a, int b);
+} // namespace trial_lib
